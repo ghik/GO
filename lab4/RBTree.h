bc1fa53f73758a552f49f11062f7cc41907ac6fa
ghik <romeqjanoosh@gmail.com>
2011-11-01 23:53:12 +0100

    lab4

---
+++ lab4/RBTree.h
@@ -0,0 +1,474 @@
+/*
+ * RBTree.h
+ *
+ *  Created on: 22-10-2011
+ *      Author: ghik
+ */
+
+#ifndef RBTREE_H_
+#define RBTREE_H_
+
+template<class T>
+class DefaultCompare {
+	int operator()(const T& val1, const T& val2) {
+		if (val1 < val2) {
+			return -1;
+		} else if (val1 > val2) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+};
+
+template<class T, class C = DefaultCompare<T> >
+class RBTree {
+public:
+	class Node;
+	typedef Node* PNode;
+
+	class Node {
+	public:
+		const T& value() {
+			return val;
+		}
+		PNode successor() {
+			return tree->successor(this);
+		}
+		PNode predecessor() {
+			return tree->predecessor(this);
+		}
+		PNode insert() {
+			tree->insert(this);
+			return this;
+		}
+		bool valid() {
+			return this != NIL;
+		}
+
+	private:
+		T val;
+		bool red;
+		PNode left;
+		PNode right;
+		PNode p;
+
+		RBTree* tree;
+
+		friend class RBTree;
+	};
+
+	RBTree();
+	~RBTree();
+
+	PNode first();
+	PNode last();
+	int size();
+	PNode create(const T& val);
+	PNode find(const T& val);
+	PNode successor(PNode n);
+	PNode predecessor(PNode n);
+	void insert(PNode n);
+	void remove(PNode n, bool swaplinks = true);
+
+	static PNode NIL;
+
+private:
+	PNode maxnode(PNode n);
+	PNode minnode(PNode n);
+	void lrotate(PNode n);
+	void rrotate(PNode n);
+	void insfixup(PNode n);
+	void remfixup(PNode n);
+	void swaplinks(PNode n1, PNode n2);
+	void destroy(PNode n);
+
+private:
+	PNode root;
+	int count;
+	C comp;
+
+	static Node nnil;
+};
+
+template<class T, class C>
+typename RBTree<T, C>::Node RBTree<T, C>::nnil;
+
+template<class T, class C>
+typename RBTree<T, C>::PNode RBTree<T, C>::NIL = 0;
+
+template<class T, class C>
+RBTree<T, C>::RBTree() {
+	if (NIL == 0) {
+		nnil.red = false;
+		nnil.left = nnil.right = nnil.p = &nnil;
+		NIL = &nnil;
+	}
+	root = NIL;
+	count = 0;
+}
+
+template<class T, class C>
+RBTree<T, C>::~RBTree() {
+	destroy(root);
+}
+
+template<class T, class C>
+int RBTree<T, C>::size() {
+	return count;
+}
+
+template<class T, class C>
+typename RBTree<T, C>::PNode RBTree<T, C>::first() {
+	return minnode(root);
+}
+
+template<class T, class C>
+typename RBTree<T, C>::PNode RBTree<T, C>::last() {
+	return maxnode(root);
+}
+
+template<class T, class C>
+typename RBTree<T, C>::PNode RBTree<T, C>::find(const T& val) {
+	PNode n = root;
+	while (n != NIL) {
+		int comp_res = comp(val, n->val);
+		if (comp_res < 0)
+			n = n->left;
+		else if (comp_res > 0)
+			n = n->right;
+		else
+			return n;
+	}
+	return NIL;
+}
+
+template<class T, class C>
+typename RBTree<T, C>::PNode RBTree<T, C>::maxnode(PNode n) {
+	if (n == NIL)
+		return NIL;
+	while (n->right != NIL)
+		n = n->right;
+	return n;
+}
+
+template<class T, class C>
+typename RBTree<T, C>::PNode RBTree<T, C>::minnode(PNode n) {
+	if (n == NIL)
+		return NIL;
+	while (n->left != NIL)
+		n = n->left;
+	return n;
+}
+
+template<class T, class C>
+typename RBTree<T, C>::PNode RBTree<T, C>::successor(PNode n) {
+	if (n->right != NIL) {
+		n = n->right;
+		while (n->left != NIL)
+			n = n->left;
+		return n;
+	}
+	PNode t = n->p;
+	while (t != NIL && n == t->right) {
+		n = t;
+		t = t->p;
+	}
+	return t;
+}
+
+template<class T, class C>
+typename RBTree<T, C>::PNode RBTree<T, C>::predecessor(PNode n) {
+	if (n->left != NIL) {
+		n = n->left;
+		while (n->right != NIL)
+			n = n->right;
+		return n;
+	}
+	PNode t = n->p;
+	while (t != NIL && n == t->left) {
+		n = t;
+		t = t->p;
+	}
+	return t;
+}
+
+template<class T, class C>
+void RBTree<T, C>::lrotate(PNode n) {
+	if (n == NIL || n->right == NIL)
+		return;
+	if (n == root)
+		root = n->right;
+	n->right->p = n->p;
+	if (n->p != NIL) {
+		if (n == n->p->left)
+			n->p->left = n->right;
+		else
+			n->p->right = n->right;
+	}
+	n->p = n->right;
+	n->right = n->right->left;
+	if (n->right != NIL)
+		n->right->p = n;
+	n->p->left = n;
+}
+
+template<class T, class C>
+void RBTree<T, C>::rrotate(PNode n) {
+	if (n == NIL || n->left == NIL)
+		return;
+	if (n == root)
+		root = n->left;
+	n->left->p = n->p;
+	if (n->p != NIL) {
+		if (n == n->p->left)
+			n->p->left = n->left;
+		else
+			n->p->right = n->left;
+	}
+	n->p = n->left;
+	n->left = n->left->right;
+	if (n->left != NIL)
+		n->left->p = n;
+	n->p->right = n;
+}
+
+template<class T, class C>
+void RBTree<T, C>::insert(PNode n) {
+	count++;
+	n->red = true;
+	n->left = n->right = NIL;
+	const T& val = n->val;
+	PNode t = root, pt = NIL;
+	while (t != NIL) {
+		pt = t;
+		if (comp(val, t->val) < 0)
+			t = t->left;
+		else
+			t = t->right;
+	}
+	if (pt != NIL) {
+		if (comp(val, pt->val) < 0)
+			pt->left = n;
+		else
+			pt->right = n;
+	}
+	n->p = pt;
+	if (root == NIL)
+		root = n;
+	insfixup(n);
+}
+
+template<class T, class C>
+void RBTree<T, C>::insfixup(PNode n) {
+	while (n->p->red)
+		if (n->p == n->p->p->left) {
+			if (n->p->p->right->red) {
+				n->p->p->red = true;
+				n->p->red = false;
+				n->p->p->right->red = false;
+				n = n->p->p;
+			} else {
+				if (n == n->p->right) {
+					n = n->p;
+					lrotate(n);
+				}
+				n->p->p->red = true;
+				n->p->red = false;
+				rrotate(n->p->p);
+			}
+		} else {
+			if (n->p->p->left->red) {
+				n->p->p->red = true;
+				n->p->red = false;
+				n->p->p->left->red = false;
+				n = n->p->p;
+			} else {
+				if (n == n->p->left) {
+					n = n->p;
+					rrotate(n);
+				}
+				n->p->p->red = true;
+				n->p->red = false;
+				lrotate(n->p->p);
+			}
+		}
+	root->red = false;
+}
+
+template<class T, class C>
+void RBTree<T, C>::swaplinks(PNode n1, PNode n2) {
+	if (n1 == n2 || n1 == NIL || n2 == NIL)
+		return;
+	if (n1 == root)
+		root = n2;
+	else if (n2 == root)
+		root = n1;
+	PNode tn;
+	bool n1leftn2 = (n1 == n2->left), n1rightn2 = (n1 == n2->right);
+	if (n2->p == n1) {
+		tn = n1;
+		n1 = n2;
+		n2 = tn;
+	}
+	if (n1->p == n2->p) {
+		tn = n1->p->left;
+		n1->p->left = n1->p->right;
+		n1->p->right = tn;
+	} else {
+		if (n1->p != NIL && !n1leftn2 && !n1rightn2) {
+			if (n1 == n1->p->right)
+				n1->p->right = n2;
+			else
+				n1->p->left = n2;
+		}
+		if (n2->p != NIL) {
+			if (n2 == n2->p->right)
+				n2->p->right = n1;
+			else
+				n2->p->left = n1;
+		}
+		tn = n1->p;
+		n1->p = n2->p;
+		if (!n1leftn2 && !n1rightn2)
+			n2->p = tn;
+		else
+			n2->p = n1;
+	}
+	if (n1->left != NIL)
+		n1->left->p = n2;
+	if (n1->right != NIL)
+		n1->right->p = n2;
+	if (n2->left != NIL && !n1leftn2)
+		n2->left->p = n1;
+	if (n2->right != NIL && !n1rightn2)
+		n2->right->p = n1;
+	tn = n2->left;
+	n2->left = n1->left;
+	if (!n1leftn2)
+		n1->left = tn;
+	else
+		n1->left = n2;
+	tn = n2->right;
+	n2->right = n1->right;
+	if (!n1rightn2)
+		n1->right = tn;
+	else
+		n1->right = n2;
+	bool tc;
+	tc = n1->red;
+	n1->red = n2->red;
+	n2->red = tc;
+}
+
+template<class T, class C>
+void RBTree<T, C>::remove(PNode n, bool swln) {
+	if (n == NIL)
+		return;
+	count--;
+	PNode tn;
+	if (n->left != NIL && n->right != NIL) {
+		tn = successor(n);
+		if (swln)
+			swaplinks(n, tn);
+		else {
+			const T& tv = n->val;
+			n->val = tn->val;
+			tn->val = tv;
+			n = tn;
+		}
+	}
+	if (n->right != NIL)
+		tn = n->right;
+	else
+		tn = n->left;
+	if (n != root) {
+		if (n == n->p->right)
+			n->p->right = tn;
+		else
+			n->p->left = tn;
+	} else
+		root = tn;
+	tn->p = n->p;
+	if (!n->red)
+		remfixup(tn);
+	if (tn == NIL)
+		tn->p = NIL;
+	delete n;
+}
+
+template<class T, class C>
+void RBTree<T, C>::remfixup(PNode tn) {
+	PNode w;
+	while (tn != root && !tn->red)
+		if (tn == tn->p->left) {
+			w = tn->p->right;
+			if (w->red) {
+				tn->p->red = true;
+				w->red = false;
+				lrotate(tn->p);
+				w = tn->p->right;
+			}
+			if (!w->right->red && !w->left->red) {
+				w->red = true;
+				tn = tn->p;
+			} else {
+				if (!w->right->red) {
+					w->red = true;
+					w->left->red = false;
+					rrotate(w);
+					w = w->p;
+				}
+				w->red = tn->p->red;
+				tn->p->red = false;
+				w->right->red = false;
+				lrotate(tn->p);
+				tn = root;
+			}
+		} else {
+			w = tn->p->left;
+			if (w->red) {
+				tn->p->red = true;
+				w->red = false;
+				rrotate(tn->p);
+				w = tn->p->left;
+			}
+			if (!w->right->red && !w->left->red) {
+				w->red = true;
+				tn = tn->p;
+			} else {
+				if (!w->left->red) {
+					w->red = true;
+					w->right->red = false;
+					lrotate(w);
+					w = w->p;
+				}
+				w->red = tn->p->red;
+				tn->p->red = false;
+				w->left->red = false;
+				rrotate(tn->p);
+				tn = root;
+			}
+		}
+	tn->red = false;
+}
+
+template<class T, class C>
+typename RBTree<T, C>::PNode RBTree<T, C>::create(const T& val) {
+	PNode n = new Node;
+	n->val = val;
+	n->tree = this;
+	return n;
+}
+
+template<class T, class C>
+void RBTree<T, C>::destroy(PNode n) {
+	if (n == NIL)
+		return;
+	destroy(n->right);
+	destroy(n->left);
+	delete n;
+	n = NIL;
+}
+
+#endif /* RBTREE_H_ */
---
+++ lab4/main.cpp
@@ -0,0 +1,220 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 20-10-2011
+ *      Author: ghik
+ */
+
+#include <iostream>
+#include <fstream>
+
+#include <algorithm>
+#include <vector>
+#include <list>
+using namespace std;
+
+enum Side {
+	NONE, LEFT, RIGHT
+};
+
+struct Point {
+	Point() :
+			x(0), y(0), side(NONE) {
+	}
+	Point(double _x, double _y) :
+			x(_x), y(_y), side(NONE) {
+	}
+	bool operator==(const Point& rhs) {
+		return x == rhs.x && y == rhs.y;
+	}
+	bool operator<(const Point& rhs) {
+		if(y == rhs.y) {
+			return x < rhs.x;
+		}
+		return y < rhs.y;
+	}
+	bool operator>(const Point& rhs) {
+		if(y == rhs.y) {
+			return x > rhs.x;
+		}
+		return y > rhs.y;
+	}
+	double x;
+	double y;
+	Side side;
+};
+
+ostream& operator<<(ostream& str, const Point& p) {
+	return str << '(' << p.x << ',' << p.y << ')';
+}
+
+istream& operator>>(istream& str, Point& p) {
+	return str >> p.x >> p.y;
+}
+
+int n;
+vector<Point> polygon;
+
+double product(const Point& p1, const Point& p2, const Point& p3) {
+	return (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
+}
+
+bool is_y_monotonous(vector<Point>& polygon) {
+	bool minfound = false, maxfound = false;
+	for(vector<Point>::iterator it=polygon.begin();it!=polygon.end();it++) {
+		Point& prev = it != polygon.begin() ? *(it-1) : *(polygon.end()-1);
+		Point& next = (it+1) != polygon.end() ? *(it+1) : *polygon.begin();
+		if(prev > *it && next > *it) {
+			if(minfound) {
+				return false;
+			}
+			minfound = true;
+		}
+		if(prev < *it && next < *it) {
+			if(maxfound) {
+				return false;
+			}
+			maxfound = true;
+		}
+	}
+	return true;
+}
+
+vector<Point>::iterator prev(vector<Point>::iterator it) {
+	return it != polygon.begin() ? it-1 : polygon.end()-1;
+}
+
+vector<Point>::iterator next(vector<Point>::iterator it) {
+	return (it+1) != polygon.end() ? it+1 : polygon.begin();
+}
+
+bool in_polygon(Point& p1, Point& p2, Point& p3) {
+	double prod = product(p1, p2, p3);
+	if(p1.side == LEFT) {
+		return prod < 0;
+	} else {
+		return prod > 0;
+	}
+}
+
+int main(int argc, char** argv) {
+	const char* file = "sq_polygon.txt";
+
+	ofstream fout("../geomtest/polygon_res.txt");
+
+	ifstream fin(file);
+	fin >> n;
+	for (int i = 0; i < n; i++) {
+		polygon.push_back(Point());
+		fin >> polygon.back();
+	}
+	fin.close();
+
+	fout << "frames 0 -1" << endl;
+	fout << "polygon ";
+	for(vector<Point>::iterator it=polygon.begin();it!=polygon.end();it++) {
+		fout << it->x << ' ' << it->y << ' ';
+	}
+	fout << endl;
+
+	if(!is_y_monotonous(polygon)) {
+		cout << "Not y-monotonous polygon!" << endl;
+		return 0;
+	}
+
+	vector<Point>::iterator it, topit, maxit, minit, lit, rit;
+
+	maxit = polygon.begin();
+	for(vector<Point>::iterator it=polygon.begin();it!=polygon.end();it++) {
+		if(*it > *maxit) {
+			maxit = it;
+		}
+	}
+
+	minit = polygon.begin();
+	for(it=polygon.begin();it!=polygon.end();it++) {
+		if(*it < *minit) {
+			minit = it;
+		}
+	}
+
+	list<vector<Point>::iterator> stack;
+	lit = rit = maxit;
+
+	it = lit;
+	while(it != minit) {
+		it = next(it);
+		it->side = LEFT;
+	}
+
+	it = rit;
+	while(it != minit) {
+		it = prev(it);
+		it->side = RIGHT;
+	}
+
+	minit->side = maxit->side = NONE;
+
+	stack.push_back(maxit);
+	if(*next(maxit) > *prev(maxit)) {
+		lit = next(maxit);
+		stack.push_back(lit);
+	} else {
+		rit = prev(maxit);
+		stack.push_back(rit);
+	}
+
+	int frame = 0;
+	list<vector<Point>::iterator>::iterator sit;
+	while(true) {
+		frame++;
+		fout << "frames " << frame << ' ' << frame << endl;
+		fout << "lineColor 1 0 0 1" << endl << "fillColor 1 0 0 1" << endl;
+		for(sit=stack.begin();sit!=stack.end();sit++) {
+			fout << "dot " << (*sit)->x << ' ' << (*sit)->y << " 2" << endl;
+		}
+
+		topit = stack.back();
+		if(*next(lit) > *prev(rit)) {
+			it = lit = next(lit);
+		} else {
+			it = rit = prev(rit);
+		}
+
+		fout << "lineColor 0 1 0 1" << endl << "fillColor 0 1 0 1" << endl;
+		fout << "dot " << it->x << ' ' << it->y << " 2" << endl;
+
+		fout << "lineColor 0 0 1 1" << endl;
+		if(it == minit) {
+			break;
+		}
+		if(it->side != stack.back()->side) {
+			stack.erase(stack.begin());
+			for(sit=stack.begin();sit!=stack.end();sit++) {
+				fout << "frames " << frame << " -1" << endl;
+				fout << "segment " << (*sit)->x << ' ' << (*sit)->y << ' ' << it->x << ' ' << it->y << endl;
+			}
+			stack.clear();
+			stack.push_back(topit);
+			stack.push_back(it);
+		} else {
+			sit = stack.end();
+			sit--; sit--;
+			while(in_polygon(*it, *topit, **sit)) {
+				fout << "frames " << frame << " -1" << endl;
+				fout << "segment " << (*sit)->x << ' ' << (*sit)->y << ' ' << it->x << ' ' << it->y << endl;
+				stack.pop_back();
+				if((*sit)->side != it->side) {
+					break;
+				}
+				topit = stack.back();
+				sit--;
+			}
+			stack.push_back(it);
+		}
+	}
+
+	fout.close();
+
+	return 0;
+}
